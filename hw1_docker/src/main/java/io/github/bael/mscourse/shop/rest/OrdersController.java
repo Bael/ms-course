73aa19432eb20d9da3be6cae7e9497a1119a5388
d.kerner <denkern@gmail.com>
2020-04-26 18:28:31 +0700

    hw2

--- hw1_docker/src/main/java/io/github/bael/mscourse/shop/rest/OrdersController.java
+++ hw1_docker/src/main/java/io/github/bael/mscourse/shop/rest/OrdersController.java
@@ -3,11 +3,16 @@ package io.github.bael.mscourse.shop.rest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class OrdersController {
 
     @GetMapping("/health")
-    public String test() {
-        return "OK";
+    public Map<String, String> test() {
+        Map<String, String> map = new HashMap<>();
+        map.put("status", "OK");
+        return map;
     }
 }
